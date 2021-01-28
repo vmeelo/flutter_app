157c2d62a2f7ce5530a4104c7437ebb5ab2a9e81
Vinicius Melo <vinicius.melo@dcomp.ufs.br>
2021-01-28 05:07:02 -0300

    Add setState and rehabilitating the buttons +1 and -1 and seeing on the counter.

--- lib/main.dart
+++ lib/main.dart
@@ -3,56 +3,92 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: "People counter: 0!",
-      home: Stack(
-        children: <Widget>[
-          Image.asset(
-            "images/buzz.jpg",
-            fit: BoxFit.cover,
-            height: 1000.0,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("Counted: 0 !",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  )),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      child: Text(
-                        "+1",
-                        style:
-                            TextStyle(fontSize: 40.0, color: Colors.white),
-                      ),
-                      onPressed: () {},
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      child: Text(
-                        "-1",
-                        style: TextStyle(fontSize: 40.0, color: Colors.green),
-                      ),
-                      onPressed: () {},
-                    ),
-                  ),
-                ],
-              ),
-              Text(
-                "Thanks !",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontStyle: FontStyle.italic,
-                    fontSize: 25.0),
-              ),
-            ],
-          )
-        ],
-      )));
+      home: Home()));
 }
+
+/* We need create a class to return an widget, this will help us to make an
+interaction with the interface. When someone click in some button (+1 or -1),
+the app will count +1 people.*/
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+
+  /*Using the function _changePeople with setState, we can rebuild the
+  * interface, and show us the number of people who touched the button.
+  * The setState will refresh the screen app. It's kinda a F5 on web page*/
+  void _changePeople(int delta){
+  setState(() {
+    _people += delta;
+  });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/buzz.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Counted: $_people !",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                )),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "+1",
+                      style:
+                      TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () {
+                     _changePeople(1);
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "-1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.green),
+                    ),
+                    onPressed: () {
+                      _changePeople(1);
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              "Thanks !",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 25.0),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+
+
+
+
