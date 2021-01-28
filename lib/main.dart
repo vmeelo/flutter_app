f1a49129714ee62a4b5fa77d43dc6799b8af23c2
Vinicius Melo <vinicius.melo@dcomp.ufs.br>
2021-01-28 01:31:28 -0300

    add images to the background.

--- lib/main.dart
+++ lib/main.dart
@@ -2,47 +2,57 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      title: "Contador de pessoas !",
-      home: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          Text("Pessoas: 0",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          )),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+      title: "People counter: 0!",
+      home: Stack(
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: FlatButton(
-              child: Text(
-                "+1",
-                style: TextStyle(fontSize: 40.0, color: Colors.white),
-              ),
-              onPressed: () {},
-            ),
+          Image.asset(
+            "images/buzz.jpg",
+            fit: BoxFit.cover,
+            height: 1000.0,
           ),
-            Padding(
-              padding: EdgeInsets.all(10.0),
-              child: FlatButton(
-                child: Text(
-                  "-1",
-                  style: TextStyle(fontSize: 40.0, color: Colors.white),
-                ),
-                onPressed: () {},
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Counted: 0 !",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  )),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "+1",
+                        style:
+                            TextStyle(fontSize: 40.0, color: Colors.white),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "-1",
+                        style: TextStyle(fontSize: 40.0, color: Colors.green),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
               ),
-            ),
+              Text(
+                "Thanks !",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 25.0),
+              ),
             ],
-          ),
-          Text(
-            "Pode entrar !",
-            style: TextStyle(
-                color: Colors.white,
-                fontStyle: FontStyle.italic,
-                fontSize: 25.0),
-          ),
+          )
         ],
       )));
 }
